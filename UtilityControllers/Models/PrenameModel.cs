6bd6ab0bd5422384f2fdd93740cfa148f5bd3379
dulyasit <dulyasit@hotmail.com>
2018-12-02 20:29:40 +0700

    แก้ไข Model  สำหรับ คำนำหน้าชื่อ

--- UtilityControllers/Models/PrenameModel.cs
+++ UtilityControllers/Models/PrenameModel.cs
@@ -7,7 +7,15 @@ namespace UtilityControllers.Models
 {
     public class PrenameModel
     {
-        public int PreNameRunno { get; set; }
+        public string PreNameID { get; set; }
         public string PreName { get; set; }
+        public string PersonFlag { get; set; }
+        public string CorporateFlag { get; set; }
+        public string Blank1 { get; set; }
+        public string Blank2 { get; set; }
+        public string Blank3 { get; set; }
+        public string Blank4 { get; set; }
+        public string Blank5 { get; set; }
+        public string Blank6 { get; set; }        
     }
 }
